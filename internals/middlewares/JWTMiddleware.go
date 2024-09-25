ae8efb3a9a6668c366f48c272fd1f65c7b388e34
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-09-25 18:42:35 +0000

    hehe

---
+++ internals/middlewares/JWTMiddleware.go
@@ -0,0 +1,45 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lordofthemind/EventureGo/internals/responses"
+	"github.com/lordofthemind/mygopher/gophertoken"
+)
+
+func AuthTokenMiddleware(tokenManager gophertoken.TokenManager) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token, err := c.Cookie("SuperUserAuthorization")
+		if err != nil {
+			response := responses.NewGinResponse(
+				c,
+				http.StatusUnauthorized,
+				"Unauthorized",
+				nil,
+				"Failed to get token from cookie",
+			)
+			c.JSON(http.StatusUnauthorized, response)
+			c.Abort()
+			return
+		}
+
+		payload, err := tokenManager.ValidateToken(token)
+		if err != nil {
+			response := responses.NewGinResponse(
+				c,
+				http.StatusUnauthorized,
+				"Invalid token",
+				nil,
+				err.Error(),
+			)
+			c.JSON(http.StatusUnauthorized, response)
+			c.Abort()
+			return
+		}
+
+		c.Set("userID", payload.ID)         // Use payload ID or other necessary field
+		c.Set("username", payload.Username) // Optionally set username if needed
+		c.Next()
+	}
+}
